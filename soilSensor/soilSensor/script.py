bfc712d75d1ab57d01375e2f2eef87395e0eb192
Linda Kjellberg <lindakjellberg7@gmail.com>
2024-01-03 16:09:54 +0100

    Update script to include webhook

--- soilSensor/soilSensor/script.py
+++ soilSensor/soilSensor/script.py
@@ -1,9 +1,12 @@
-import serial
-import time
+import serial  # Add this import for the serial module
+import time  # Add this import for the time module
 from dotenv import load_dotenv
 import os
 import mysql.connector
 import re  # Import the regular expression module
+from datetime import datetime, timedelta
+import requests
+
 
 # Load environment variables from .env file
 load_dotenv()
@@ -14,9 +17,21 @@ DB_PASSWORD = os.getenv('DB_PASSWORD', '')
 DB_HOST = os.getenv('DB_HOST', '')
 DB_DATABASE = os.getenv('DB_DATABASE', '')
 
+# Discord webhook URL
+DISCORD_WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_URL', '')
+
 # Serial port configuration
 SERIAL_PORT = os.getenv('SERIAL_PORT', '')
 BAUD_RATE = int(os.getenv('BAUD_RATE', '9600'))
+
+# Humidity threshold for notification
+HUMIDITY_THRESHOLD = 25
+
+# Time interval for Discord notifications (in seconds)
+# NOTIFICATION_INTERVAL = 24 * 60 * 60  # 24 hours
+
+# Time tracking for notifications
+last_notification_time = datetime.now()
 
 # Open serial port
 ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
@@ -34,10 +49,11 @@ try:
 
     cursor = connection.cursor()
 
+
     while True:
         # Read data from Arduino Uno
         line = ser.readline().decode('utf-8').strip()
-        
+
         # Extract numeric value from the printed content
         humidity_match = re.search(r"Soil Humidity: (\d+)%", line)
         if humidity_match:
@@ -50,7 +66,19 @@ try:
 
             print(f"Inserted humidity value: {humidity_value}")
 
-        time.sleep(60)  # Send data every 60 second
+            # Check if humidity is below the threshold
+            if humidity_value <= HUMIDITY_THRESHOLD:
+                print(f"Alert: Soil humidity is below {HUMIDITY_THRESHOLD}% - Sending notification to Discord")
+                    
+                # Create message content
+                message_content = f"Alert: Soil humidity is below {HUMIDITY_THRESHOLD}%. Current humidity {humidity_value}%"
+
+                # Send notification to Discord using webhook
+                payload = {"content": message_content}
+                requests.post(DISCORD_WEBHOOK_URL, json=payload)
+
+
+        time.sleep(60)  # Send data every 60 seconds
 
 except KeyboardInterrupt:
     print("Script terminated by user")
@@ -65,4 +93,3 @@ finally:
     if connection.is_connected():
         cursor.close()
         connection.close()
-
