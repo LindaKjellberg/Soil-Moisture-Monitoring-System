babfffd99d4a65bfd9af1eec93be6bc4e8e85edf
Linda Kjellberg <lindakjellberg7@gmail.com>
2024-01-04 07:25:44 +0100

    Update comments

--- soilSensor/soilSensor/soilSensor.ino
+++ soilSensor/soilSensor/soilSensor.ino
@@ -1,9 +1,7 @@
-
 #include <LiquidCrystal.h>  // Include the Adafruit library for the LCD
-//#include "secrets.h"  // Include the secret header file with AWS IOT Core certificates
 
 
-// Setup values for Capacitive Soil Sensor, this particular sensor has a range of 424 to 796
+// Setup values for Capacitive Soil Sensor, sensor has a range of 424 to 796
 const int dry = 796;  // Value for dry sensor, the highest possible value the sensor can measure
 const int wet = 424;  // Value for wet sensor, the lowest possible value the sensor can measure
 
@@ -16,8 +14,8 @@ const int d5 = 4;   // LCD pin d5 connected to Arduino digital pin 4
 const int d6 = 3;   // LCD pin d6 connected to Arduino digital pin 3
 const int d7 = 2;   // LCD pin d7 connected to Arduino digital pin 2
 
-// Instantiate LiquidCrystal class (from library) with name lcd
-LiquidCrystal lcd(rs, vo, e, d4, d5, d6, d7);  // Instantiate LCD using the pins as arguments to setup the communication between the LCD display and the Arduino Uno board
+// Configure LCD display connection to Arduino board
+LiquidCrystal lcd(rs, vo, e, d4, d5, d6, d7);  // Instantiate LCD with the pins as arguments to setup the communication
 
 // Setup that prepares and starts the program
 void setup() {
@@ -32,29 +30,19 @@ void loop() {
   int sensorValue = analogRead(A0);  // Get the measured sensor value as an integer from the analog pin A0
 
   // Translate the range of values to a scale of 0% to 100% for a more evident value representation
-  int percentageHumidity = map(sensorValue, wet, dry, 100, 0);  // Using the map function to translate sensor value to humidity in percent
+  int percentageHumidity = map(sensorValue, wet, dry, 100, 0);  // Mapping the sensor value to humidity in percent
 
   // Print to terminal
-  Serial.print("Soil Humidity: ");   // Print a message that clarifies what is being printed to the terminal (without a line break to get the data on the same line)
-  Serial.print(percentageHumidity);  // Print the humidity value in percent to the terminal (without a line break to get the percentage symbol on the same line)
-  Serial.println("%");               // Add percentage symbol at the end of each value (with a line break to start on an new line with the next print)
+  Serial.print("Soil Humidity: ");   // Print the text message to the LCD
+  Serial.print(percentageHumidity);  // Print the humidity value in percent to the terminal
+  Serial.println("%");               // Add percentage symbol at the end of each value
 
   // Print to LCD Screen
   lcd.setCursor(1, 0);            // Moves the cursor to column 2 on the first row
-  lcd.print("Soil Humidity: ");   // Print a message that clarifies what is being printed to the LCD (without a line break to get the data on the same line)
+  lcd.print("Soil Humidity: ");   // Print the text message to the LCD
   lcd.setCursor(6, 1);            // Moves the cursor to column 7 on the second row
-  lcd.print(percentageHumidity);  // Print the humidity to LCD (without a line break to get the percentage symbol on the same line)
-  lcd.print("%");                 // Add percentage symbol at the end of each value (with a line break to start on an new line with the next print)
+  lcd.print(percentageHumidity);  // Print the humidity to LCD
+  lcd.print("%");                 // Add percentage symbol at the end of each value
 
   delay(1000);  // One second delay in between each value reading for a clear representation
-
-  /*
- // Turn off display
- lcd.noDisplay();
- delay(1000);
-
- //Turn on display
- lcd.display();
- delay(1000);
- */
 }
